2ce325aae4a61793373ec9f356c69168711d8887
carsonng <ngcarson@gmail.com>
2023-02-22 00:59:08 -0700

    Add onClick function.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,11 +13,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick (View view) {
+        TextView txtHello = findViewById(R.id.txtMsg);
+        txtHello.setText("B");
     }
 }
