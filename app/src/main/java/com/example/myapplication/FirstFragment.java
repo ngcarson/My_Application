d06170177bd340681d564812bc2bc572787b8a1d
carsonng <ngcarson@gmail.com>
2023-03-10 14:15:42 -0700

    Added text-to-speech functionality.

--- app/src/main/java/com/example/myapplication/FirstFragment.java
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -1,12 +1,16 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import java.util.Locale;
 
 public class FirstFragment extends AppCompatActivity {
+    private TextToSpeech mTTS;
     static String[] letters = {"A","B","C","D","E","F"};
     int letter_position = 0;
 
@@ -15,15 +19,49 @@ public class FirstFragment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_first);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.CANADA);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e( "TTS", "Language not supported");
+                    } else {
+                        //test
+                    }
+                } else {
+                    Log.e( "TTS", "Initialization failed");
+                }
+            }
+        });
     }
 
     public void onClick (View view) {
         letter_position += 1;
         TextView txtHello = findViewById(R.id.txtMsg);
         txtHello.setText(letters[letter_position]);
+        speak(letters[letter_position]);
         if (letter_position == 5) {
             letter_position = -1;
         }
+    }
+
+    public void speak(String letter) {
+        String text = letter;
+
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+
+        super.onDestroy();
     }
 
 }
