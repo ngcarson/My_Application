66055fffd8f72f0e7718aa956b5ffd9da087ef66
carsonng <ngcarson@gmail.com>
2023-03-11 14:37:53 -0700

    Added code template for ABC RANDOM for button #2.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,11 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Button #1: ABC IN ORDER
         button = (Button) findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (MainActivity.this,FirstFragment.class);
+                startActivity(intent);
+            }
+        });
+
+        // Button #2: ABC RANDOM
+        button = (Button) findViewById(R.id.btn2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this,SecondFragment.class);
                 startActivity(intent);
             }
         });
